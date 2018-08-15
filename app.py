fbdd6db4101d7ef1cafb748c6222d32cc2d7c1ef
Robin <robin.du@dataraft.com.au>
2018-08-15 10:31:47 +1000

    closing displayModeBar

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ app.layout = html.Div([
                     # Insert Scatter and Box plot...
                     html.Div([
                         html.Div( #Insert Scatter plot...
-                            dcc.Graph(id='scatter-plot'),
+                            dcc.Graph(id='scatter-plot',config=dict(displayModeBar=False)),
                             className='col-lg-12 col-md-12 col-sm-12 col-xs-12'),
                         html.Br([]),
                         html.Div( #Insert Box plot...
@@ -130,7 +130,7 @@ app.layout = html.Div([
                                   animate=True)
                     ]),
                     html.Div([
-                        html.H6('About DataRaft'),
+                        html.H6('About DataRaft', config=dict(displayModeBar=False)),
                         html.P('DataRaft provides services in Customer Strategy and Data Analytics.  Our unique value comes from our domain expertise from experience working in senior strategy and marketing roles for ASX 200 organisations.'),
                         html.Br([])
                     ], style={'color': 'white'})
